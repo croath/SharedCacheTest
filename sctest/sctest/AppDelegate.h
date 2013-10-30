ab8afb6acce6b38ff5c6b69aa7984484fa3a0a70
Croath <croathliu@gmail.com>
2013-10-30 22:57:15 +0800

    basic files added

---
+++ sctest/sctest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sctest
+//
+//  Created by croath on 10/30/13.
+//  Copyright (c) 2013 Croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sctest/sctest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sctest
+//
+//  Created by croath on 10/30/13.
+//  Copyright (c) 2013 Croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
